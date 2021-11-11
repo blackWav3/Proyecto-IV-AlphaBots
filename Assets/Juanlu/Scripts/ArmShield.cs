363e10ef112447b1fd18f9e344a0da4eeb2d8204
Juanluich <44837619+Juanluich@users.noreply.github.com>
2021-11-11 12:45:18 +0100

    Escudo y Salto
    Arreglos de bugs del escudo y del salto

--- Assets/Juanlu/Scripts/ArmShield.cs
+++ Assets/Juanlu/Scripts/ArmShield.cs
@@ -10,12 +10,16 @@ public class ArmShield : MonoBehaviour
     public int lifeShield;
     bool activatedShield;
 
+    bool spam = false;
+    
+
     //uwu cool down del escudo
     public int cooldown; //<--esto se lo pones al yield return del coroutine y vas volando
 
     // Start is called before the first frame update
     void Start()
     {
+
         shieldAnimation = GetComponent<Animator>();
     }
 
@@ -32,35 +36,53 @@ public class ArmShield : MonoBehaviour
         }
 
         //Activar habilidad (solo si la vida no es 0)
-        if (Input.GetMouseButtonDown(0) && activatedShield == false && lifeShield !=0)
+        if (Input.GetMouseButtonDown(0) && activatedShield == false && lifeShield !=0 && spam == false)
         {
             shieldAnimation.SetBool("shield", true); //Activamos la animación de sacar el escudo
 
             StartCoroutine("Delay");
 
-            
-
-            Player.GetComponent<Player>().speed = Player.GetComponent<Player>().speed / 2;  //Reducimos la velocidad a la mitad cuando el escudo esté activo
+            spam = true;
+            StartCoroutine("ShieldSpam");
 
             Debug.Log("Activado");
+            
         }
-        else if (Input.GetMouseButtonDown(0))
+        else if (Input.GetMouseButtonDown(0) )
         {
             shieldAnimation.SetBool("shield", false);
             activatedShield = false;
             shield.SetActive(false);
+
+            
             Debug.Log("Desactivado");
 
-            Player.GetComponent<Player>().speed = Player.GetComponent<Player>().speed * 2;  //Reestablecemos la velocidad
+            Player.GetComponent<Player>().speed = Player.GetComponent<Player>().maxSpeed;  //Reestablecemos la velocidad
+        }
 
+        if (lifeShield <= 0)
+        {
+            Player.GetComponent<Player>().speed = Player.GetComponent<Player>().maxSpeed;
         }
     }
 
     IEnumerator Delay()
     {
-        yield return new WaitForSeconds(1.25f);
+        yield return new WaitForSeconds(1);
         shield.SetActive(true); //Hacemos aparecer el escudo
+
+        Player.GetComponent<Player>().speed = Player.GetComponent<Player>().speed / 2;  //Reducimos la velocidad a la mitad cuando el escudo esté activo
+
         activatedShield = true; //El estado del escudo ahora es activo
+    }
+    
+
+    //Cd para no spamear el escudo una vez lo desactivas
+    IEnumerator ShieldSpam()
+    {
+        yield return new WaitForSeconds(4);
+        spam = false; 
+        
     }
 
     //CD escudo
