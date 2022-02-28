10213b91763e1791a49ef5350e1712f1f1406af0
jaumet2000 <jaumet2000@users.noreply.github.com>
2022-02-28 17:46:38 +0100

    disparo con variables

---
+++ Assets/Jaime/disparos.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class disparos : MonoBehaviour
+{
+    PhotonView PV;
+    public Transform Muzzle;
+    public int cantidad;
+    public float TiempoEspaciado;
+    public float velocidadaBala;
+    public GameObject prefabBala;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    void OnMouseDown()
+    {
+        if (!PV.IsMine) return;
+        PV.RPC("disparo", RpcTarget.AllBuffered);
+    }
+
+    [PunRPC]
+    void disparo()
+    {
+        StartCoroutine(disparacion());
+        
+    }
+
+    IEnumerator disparacion()
+    {
+        for (int i = 0; i <= cantidad; i++)
+        {
+            GameObject balaActual = PhotonNetwork.Instantiate("prefabBala", Muzzle.transform.position, Quaternion.identity);
+            balaActual.GetComponent<Rigidbody>().AddForce(transform.forward * velocidadaBala);
+            yield return new WaitForSeconds(TiempoEspaciado);
+        }
+    }
+}
