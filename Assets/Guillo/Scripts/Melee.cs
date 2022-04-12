89b0d7557cf800d56752b9fcfdf5cf380abf9ab7
Guillo77 <gcmarti15@gmail.com>
2022-04-12 12:54:29 +0200

    mele

--- Assets/Guillo/Scripts/Melee.cs
+++ Assets/Guillo/Scripts/Melee.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class Melee : MonoBehaviour
 {
+    public Brazos brazos;
     public Animator animator;
-    public Transform attackPoint;
+    public Transform martilloAttackPoint;
+    public Transform firstAttackPointE, secondAttackPointE, centerPointE;
+    public Transform firstAttackPointS, secondAttackPointS, centerPointS;
     public float attackRange;
     public float fireRate;
     public int meleeDamage;
@@ -13,6 +17,13 @@ public class Melee : MonoBehaviour
 
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            brazos = Brazos.Martillo;
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+            brazos = Brazos.Espada;
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+            brazos = Brazos.Sierra;
+
         if (Input.GetButton("Fire1") && Time.time > nextAtttackTime)
         {
             Attack();
@@ -23,21 +34,94 @@ public class Melee : MonoBehaviour
         nextAtttackTime = Time.time + fireRate;
         animator.SetTrigger("Attack");
 
-        Collider[] hitEnemy = Physics.OverlapSphere(attackPoint.position, attackRange);
-
-        if (hitEnemy == null)
-            return;
-
-        foreach (Collider enemy in hitEnemy)
+        switch (brazos)
         {
-            if (enemy.gameObject.CompareTag("Player"))
-                enemy.gameObject.GetComponent<Estados>().Daño(meleeDamage);
+            case Brazos.Martillo:
+                Collider[] hitEnemyM = Physics.OverlapSphere(martilloAttackPoint.position, attackRange);
+
+                if (hitEnemyM == null)
+                    return;
+
+                foreach (Collider enemy in hitEnemyM)
+                {
+                    if (enemy.gameObject.CompareTag("Player"))
+                        enemy.gameObject.GetComponent<Estados>().Daño(meleeDamage);
+                }
+                return;
+            case Brazos.Espada:
+                Collider[] hitEnemyE = Physics.OverlapCapsule(firstAttackPointE.position, secondAttackPointE.position, attackRange);
+
+                if (hitEnemyE == null)
+                    return;
+
+                foreach (Collider enemy in hitEnemyE)
+                {
+                    if (enemy.gameObject.CompareTag("Player"))
+                        enemy.gameObject.GetComponent<Estados>().Daño(meleeDamage);
+                }
+                return;
+            case Brazos.Sierra:
+                Collider[] hitEnemyS = Physics.OverlapCapsule(firstAttackPointS.position, secondAttackPointS.position, attackRange);
+
+                if (hitEnemyS == null)
+                    return;
+
+                foreach (Collider enemy in hitEnemyS)
+                {
+                    if (enemy.gameObject.CompareTag("Player"))
+                        enemy.gameObject.GetComponent<Estados>().Daño(meleeDamage);
+                }
+                return;
+            default:
+                return;
         }
     }
     private void OnDrawGizmosSelected()
     {
-        if (attackPoint == null)
-            return;
-        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+        switch (brazos)
+        {
+            case Brazos.Martillo:
+                if (martilloAttackPoint == null)
+                    return;
+                Gizmos.DrawWireSphere(martilloAttackPoint.position, attackRange);
+                return;
+            case Brazos.Espada:
+                DrawWireCapsule(centerPointE.position, martilloAttackPoint.rotation, 0.2f, 1f);
+                return;
+            case Brazos.Sierra:
+                DrawWireCapsule(centerPointS.position, martilloAttackPoint.rotation, 0.2f, 1f);
+                return;
+        }
+    }
+    public static void DrawWireCapsule(Vector3 _pos, Quaternion _rot, float _radius, float _height, Color _color = default(Color))
+    {
+        if (_color != default(Color))
+            Handles.color = _color;
+        Matrix4x4 angleMatrix = Matrix4x4.TRS(_pos, _rot, Handles.matrix.lossyScale);
+        using (new Handles.DrawingScope(angleMatrix))
+        {
+            var pointOffset = (_height - (_radius * 2)) / 2;
+
+            //draw sideways
+            Handles.DrawWireArc(Vector3.up * pointOffset, Vector3.left, Vector3.back, -180, _radius);
+            Handles.DrawLine(new Vector3(0, pointOffset, -_radius), new Vector3(0, -pointOffset, -_radius));
+            Handles.DrawLine(new Vector3(0, pointOffset, _radius), new Vector3(0, -pointOffset, _radius));
+            Handles.DrawWireArc(Vector3.down * pointOffset, Vector3.left, Vector3.back, 180, _radius);
+            //draw frontways
+            Handles.DrawWireArc(Vector3.up * pointOffset, Vector3.back, Vector3.left, 180, _radius);
+            Handles.DrawLine(new Vector3(-_radius, pointOffset, 0), new Vector3(-_radius, -pointOffset, 0));
+            Handles.DrawLine(new Vector3(_radius, pointOffset, 0), new Vector3(_radius, -pointOffset, 0));
+            Handles.DrawWireArc(Vector3.down * pointOffset, Vector3.back, Vector3.left, -180, _radius);
+            //draw center
+            Handles.DrawWireDisc(Vector3.up * pointOffset, Vector3.up, _radius);
+            Handles.DrawWireDisc(Vector3.down * pointOffset, Vector3.up, _radius);
+
+        }
+    }
+    public enum Brazos
+    {
+        Martillo = 0,
+        Espada = 1,
+        Sierra = 2,
     }
 }
--- Assets/Guillo/Scripts/PlayerCombat.cs
+++ Assets/Guillo/Scripts/PlayerCombat.cs
@@ -6,8 +6,35 @@ public class PlayerCombat : Melee
     private void Start()
     {
         animator = GetComponent<Animator>();
-        attackRange = 0.6f;
-        fireRate = 1;
-        meleeDamage = 20;
+    }
+    private void Update()
+    {
+        if (brazos == Brazos.Martillo)
+        {
+            attackRange = 0.38f;
+            fireRate = 1;
+            meleeDamage = 5;
+            transform.GetChild(0).GetChild(1).GetChild(0).gameObject.SetActive(true);
+            transform.GetChild(0).GetChild(1).GetChild(1).gameObject.SetActive(false);
+            transform.GetChild(0).GetChild(1).GetChild(2).gameObject.SetActive(false);
+        }
+        if (brazos == Brazos.Espada)
+        {
+            attackRange = 0.2f;
+            fireRate = 3;
+            meleeDamage = 10;
+            transform.GetChild(0).GetChild(1).GetChild(0).gameObject.SetActive(false);
+            transform.GetChild(0).GetChild(1).GetChild(1).gameObject.SetActive(true);
+            transform.GetChild(0).GetChild(1).GetChild(2).gameObject.SetActive(false);
+        }
+        if (brazos == Brazos.Sierra)
+        {
+            attackRange = 0.2f;
+            fireRate = 1;
+            meleeDamage = 20;
+            transform.GetChild(0).GetChild(1).GetChild(0).gameObject.SetActive(false);
+            transform.GetChild(0).GetChild(1).GetChild(1).gameObject.SetActive(false);
+            transform.GetChild(0).GetChild(1).GetChild(2).gameObject.SetActive(true);
+        }
     }
 }
