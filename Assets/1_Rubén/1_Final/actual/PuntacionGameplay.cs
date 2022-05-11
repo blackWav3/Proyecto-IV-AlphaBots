26faab8d4bf7e431fd32d728eec4c9334a34ed14
jaumet2000 <jaumet2000@users.noreply.github.com>
2022-05-11 18:16:13 +0200

    Animación Final victoria

--- Assets/1_Rubén/1_Final/actual/PuntacionGameplay.cs
+++ Assets/1_Rubén/1_Final/actual/PuntacionGameplay.cs
@@ -23,42 +23,148 @@ public class PuntacionGameplay : MonoBehaviour
 
     public GameObject camaraVictoria;
 
+    public GameObject jugador1;
+    public GameObject jugador2;
+    public GameObject jugador3;
+    public GameObject jugador4;
+    public GameObject jugador5;
+    public GameObject jugador6;
+
+    public GameObject canvasDesaparecer;
+    public GameObject textoVictoriaRojos;
+    public GameObject textoVictoriaAzules;
+
+    
+    
+    public Animator animacion;
+
+
     private void Start()
     {
         int_score1 = 0;
         int_score2 = 0;
+
+        
+
+        jugador1 = GameObject.Find("1(Clone)");
+
+        if (GameObject.Find("2(Clone)") != null)
+        { 
+            jugador2 = GameObject.Find("2(Clone)");
+        }
+
+        if (GameObject.Find("3(Clone)") != null)
+        {
+            jugador3 = GameObject.Find("3(Clone)");
+        }
+
+        if (GameObject.Find("4(Clone)") != null)
+        {
+            jugador4 = GameObject.Find("4(Clone)");
+        }
+
+        if (GameObject.Find("5(Clone)") != null)
+        {
+            jugador5 = GameObject.Find("5(Clone)");
+        }
+
+        if (GameObject.Find("6(Clone)") != null)
+        {
+            jugador6 = GameObject.Find("6(Clone)");
+        }
+
+
+
         StartCoroutine(CuentaAtras());
     }
 
     IEnumerator CuentaAtras()
     {
-        for(int i = tiempoPartida; i > 0; i--)
+        for(int i = tiempoPartida; i >= 0; i--)
         {
             txt_time.text = i.ToString();
             yield return new WaitForSeconds(1f);
         }
+        tiempoPartida = 0;
     }
+    
 
-
-    private void Update()
+     void Update()
     {
         txt_score1.text = int_score1.ToString();
         txt_score2.text = int_score2.ToString();
 
         if (tiempoPartida<=0 ) //poner tambien si se llega a x puntuacion
         {
+
             if (int_score1>int_score2) //gana equipo azul
             {
-                GameObject.Find("1(Clone)").GetComponent<PJ_movement>().CanTP = true;
-                GameObject spwanPosition = posicionGANADOR1;
-                GameObject.Find("1(Clone)").transform.parent = posicionGANADOR1.transform;
-                GameObject.Find("1(Clone)").transform.position = new Vector3(0,0,0);
+                
+                jugador4.GetComponent<PJ_movement>().CanTP = true;
+                jugador4.transform.SetParent(posicionGANADOR1.transform);
+                jugador4.transform.position = posicionGANADOR1.transform.position;
+
+                jugador5.GetComponent<PJ_movement>().CanTP = true;
+                jugador5.transform.SetParent(posicionGANADOR2.transform);
+                jugador5.transform.position = posicionGANADOR2.transform.position;
+
+                jugador6.GetComponent<PJ_movement>().CanTP = true;
+                jugador6.transform.SetParent(posicionGANADOR3.transform);
+                jugador6.transform.position = posicionGANADOR3.transform.position;
+
+
+                jugador1.GetComponent<PJ_movement>().CanTP = true;
+                jugador1.transform.SetParent(posicionPERDEDOR1.transform);
+                jugador1.transform.position = posicionPERDEDOR1.transform.position;
+
+                jugador2.GetComponent<PJ_movement>().CanTP = true;
+                jugador2.transform.SetParent(posicionPERDEDOR2.transform);
+                jugador2.transform.position = posicionPERDEDOR2.transform.position;
+
+                jugador3.GetComponent<PJ_movement>().CanTP = true;
+                jugador3.transform.SetParent(posicionPERDEDOR3.transform);
+                jugador3.transform.position = posicionPERDEDOR3.transform.position;
+
+                canvasDesaparecer.SetActive(false);
+                textoVictoriaAzules.SetActive(true);
                 camaraVictoria.SetActive(true);
+
+                animacion.Play("ANIMACIONPUERTA");
             }
 
             if (int_score2 > int_score1) //gana equipo rojo
             {
+                jugador1.GetComponent<PJ_movement>().CanTP = true;
+                jugador1.transform.SetParent(posicionGANADOR1.transform);
+                jugador1.transform.position = posicionGANADOR1.transform.position;
 
+                jugador2.GetComponent<PJ_movement>().CanTP = true;
+                jugador2.transform.SetParent(posicionGANADOR2.transform);
+                jugador2.transform.position = posicionGANADOR2.transform.position;
+
+                jugador3.GetComponent<PJ_movement>().CanTP = true;
+                jugador3.transform.SetParent(posicionGANADOR3.transform);
+                jugador3.transform.position = posicionGANADOR3.transform.position;
+
+
+                jugador4.GetComponent<PJ_movement>().CanTP = true;
+                jugador4.transform.SetParent(posicionPERDEDOR1.transform);
+                jugador4.transform.position = posicionPERDEDOR1.transform.position;
+
+                jugador5.GetComponent<PJ_movement>().CanTP = true;
+                jugador5.transform.SetParent(posicionPERDEDOR2.transform);
+                jugador5.transform.position = posicionPERDEDOR2.transform.position;
+
+                jugador6.GetComponent<PJ_movement>().CanTP = true;
+                jugador6.transform.SetParent(posicionPERDEDOR3.transform);
+                jugador6.transform.position = posicionPERDEDOR3.transform.position;
+
+
+                canvasDesaparecer.SetActive(false);
+                textoVictoriaRojos.SetActive(true);
+                camaraVictoria.SetActive(true);
+
+                animacion.Play("ANIMACIONPUERTA");
             }
         }
 
