0b65a090819c4f9d96feacf19a7af2d990a61e1c
borjamc <borjacofrentino@gmail.com>
2022-05-18 17:38:32 +0200

    Update DamageManager.cs

--- Assets/Jaime/Daño flotante/DamageManager.cs
+++ Assets/Jaime/Daño flotante/DamageManager.cs
@@ -6,10 +6,14 @@ public class DamageManager : MonoBehaviour
 {
     public GameObject DañoFlotante;
     public int dañoRealizado;
+    public bala balaScript;
 
+    private void Start()
+    {
+        balaScript = GetComponent<bala>();
+    }
     public void mostrarDañoRealizado(Transform spawnPadre)
     {
-        Debug.Log("void");
         GameObject X = Instantiate(DañoFlotante, spawnPadre.transform.position, Quaternion.identity, spawnPadre);
         X.GetComponent<DamageMarker>().HacerDaño(dañoRealizado);
     }
@@ -18,14 +22,14 @@ public class DamageManager : MonoBehaviour
     {
         if (other.gameObject.tag=="Player")
         {
-            if (this.gameObject.name == GameObject.Find("4(Clone)").name || this.gameObject.name == GameObject.Find("5(Clone)").name || this.gameObject.name == GameObject.Find("6(Clone)").name)
+            if (balaScript.parentName == GameObject.Find("4(Clone)").name || balaScript.parentName == GameObject.Find("5(Clone)").name || balaScript.parentName == GameObject.Find("6(Clone)").name)
             {
                 if (other.gameObject.name == GameObject.Find("1(Clone)").name || other.gameObject.name == GameObject.Find("2(Clone)").name || other.gameObject.name == GameObject.Find("3(Clone)").name)
                 {
                     mostrarDañoRealizado(other.gameObject.transform.Find("DañoVolador"));
                 }
             }
-            else if (this.gameObject.name == GameObject.Find("1(Clone)").name || this.gameObject.name == GameObject.Find("2(Clone)").name || this.gameObject.name == GameObject.Find("3(Clone)").name)
+            else if (balaScript.parentName == GameObject.Find("1(Clone)").name || balaScript.parentName == GameObject.Find("2(Clone)").name || balaScript.parentName == GameObject.Find("3(Clone)").name)
             {
                 if (other.gameObject.name == GameObject.Find("4(Clone)").name || other.gameObject.name == GameObject.Find("5(Clone)").name || other.gameObject.name == GameObject.Find("6(Clone)").name)
                 {
