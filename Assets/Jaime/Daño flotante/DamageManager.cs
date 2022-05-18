f3c077fc350a293c746abf048466a32ac5388da0
borjamc <borjacofrentino@gmail.com>
2022-05-18 18:05:40 +0200

    Revert "Preuba daño amigo"
    This reverts commit 513b249c621a5e2eb8a358064eba22c38b7e9402.

--- Assets/Jaime/Daño flotante/DamageManager.cs
+++ Assets/Jaime/Daño flotante/DamageManager.cs
@@ -6,7 +6,12 @@ public class DamageManager : MonoBehaviour
 {
     public GameObject DañoFlotante;
     public int dañoRealizado;
+    public bala balaScript;
 
+    private void Start()
+    {
+        balaScript = GetComponent<bala>();
+    }
     public void mostrarDañoRealizado(Transform spawnPadre)
     {
         GameObject X = Instantiate(DañoFlotante, spawnPadre.transform.position, Quaternion.identity, spawnPadre);
@@ -17,7 +22,21 @@ public class DamageManager : MonoBehaviour
     {
         if (other.gameObject.tag=="Player")
         {
-            mostrarDañoRealizado(other.gameObject.transform.Find("DañoVolador"));
+            if (balaScript.parentName == GameObject.Find("4(Clone)").name || balaScript.parentName == GameObject.Find("5(Clone)").name || balaScript.parentName == GameObject.Find("6(Clone)").name)
+            {
+                if (other.gameObject.name == GameObject.Find("1(Clone)").name || other.gameObject.name == GameObject.Find("2(Clone)").name || other.gameObject.name == GameObject.Find("3(Clone)").name)
+                {
+                    mostrarDañoRealizado(other.gameObject.transform.Find("DañoVolador"));
+                }
+            }
+            else if (balaScript.parentName == GameObject.Find("1(Clone)").name || balaScript.parentName == GameObject.Find("2(Clone)").name || balaScript.parentName == GameObject.Find("3(Clone)").name)
+            {
+                if (other.gameObject.name == GameObject.Find("4(Clone)").name || other.gameObject.name == GameObject.Find("5(Clone)").name || other.gameObject.name == GameObject.Find("6(Clone)").name)
+                {
+                    mostrarDañoRealizado(other.gameObject.transform.Find("DañoVolador"));
+                }
+            }
+
         }
     }
 }
